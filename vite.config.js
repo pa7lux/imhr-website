483d037d3ceb40d20f4b21f1562c769d9f7d07ba
pa7lux <pa7lux@gmail.comx>
2022-10-02 22:45:04 +0400

    added path to about to config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
         sasha: resolve(__dirname, "sasha.html"),
         vika: resolve(__dirname, "vika.html"),
         maks: resolve(__dirname, "maks.html"),
